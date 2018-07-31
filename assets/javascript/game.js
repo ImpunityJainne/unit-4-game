c4c34cfb9bc8c037acfd6b81247ba64fe0b3f634
Mandie Kramer <37544463+ImpunityJainne@users.noreply.github.com>
2018-07-31 09:51:54 -0500

    We've achieved working game!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,23 +7,37 @@ $("#score").text(score);
 
 // For #random-number, generate a random number between 19 and 120.
 
-var randomNumber = Math.floor(Math.random() * 101) + 19;
+var randomNumber = 0;
 $("#random-number").text(randomNumber);
 
 // For each of the four crystals, generate a numer between 1 and 12.
 
-var diamond = Math.floor(Math.random() * 12) + 1;
-var ruby = Math.floor(Math.random() * 12) + 1;
-var emerald = Math.floor(Math.random() * 12) + 1;
-var sapphire = Math.floor(Math.random() * 12) + 1;
+var diamond = 0;
+var ruby = 0;
+var emerald = 0;
+var sapphire = 0;
 
-console.log("Diamond value is now " + diamond);
-console.log("Ruby value is now " + ruby);
-console.log("emerald value is now " + emerald);
-console.log("sapphire value is now " + sapphire);
+// !! I broke it! Figure out how to get this function to run.
+function setGame () {
+    diamond = Math.floor(Math.random() * 12) + 1;
+    ruby = Math.floor(Math.random() * 12) + 1;
+    emerald = Math.floor(Math.random() * 12) + 1;
+    sapphire = Math.floor(Math.random() * 12) + 1;
+    randomNumber = Math.floor(Math.random() * 101) + 19;
+    score = 0;
+    $("#score").text(score);
+    $("#random-number").text(randomNumber); 
+    console.log("Diamond value is now " + diamond);
+    console.log("Ruby value is now " + ruby);
+    console.log("emerald value is now " + emerald);
+    console.log("sapphire value is now " + sapphire);
+}
+
+//sets game up to be played.
+setGame ();
 
 // When any of the jewels are clicked, add the amount of the gem to the total score.
-
+// !! Should I use a for loop here?
 
 $("#diamond").on("click", function () {
     score = score + diamond;
@@ -59,16 +73,11 @@ function checkScore() {
         wins++;
         $("#winsTally").text("Wins: " + wins);
         $("#gameStatus").text("You've won!");
-        score = 0;
+        setGame();
     } else if (score !== 0 && score > randomNumber) { // If score goes over random number, ad 1 to losses.
         losses++;
         $("#lossesTally").text("Losses: " + losses);
         $("#gameStatus").text("You've lost.");
-        score = 0;
-    }
+        setGame();
+    };
 }
-
-
-
-
-// If the total score is greater than randomNumber, add 1 to losses and refresh the game.
